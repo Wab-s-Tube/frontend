ff2c6e3487c293cea412990481f681d0ca48fbb2
wabowen89 <wabowen89@gmail.com>
2023-09-27 14:43:46 -0700

    added Landing page with boilerplate. Updated App.js with routes and Landing page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import logo from './logo.svg';
+import logo from "./logo.svg";
+import { Routes } from "react-router-dom"
 
 // components
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-
-  <>
-  
-  </>
-
+    <>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Landing />}
+        </Routes>
+      <Footer />
+    </>
   );
 }
 
