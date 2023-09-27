6979f3c4ee1f55e7fc6a5acf32fd4e7750de0b1a
wabowen89 <wabowen89@gmail.com>
2023-09-27 15:35:17 -0700

    added react-transition-group. added initial menu option to header compoent

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navbar, Nav, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
 
 const Header = () => {
 
-    
-}
+  const [openMenu, setMenu] = useState(false)
+
+  const menuToggle = () => {
+    setMenu(!openMenu)
+  }
+
+  return (
+    <>
+      <Navbar id="header-nav">
+        <Dropdown nav isOpen={openMenu} toggle={menuToggle}>
+          <DropdownToggle nav caret>
+            Menu
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem>Test 1</DropdownItem>
+            <DropdownItem>Test 2</DropdownItem>
+            <DropdownItem>Test 3</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </Navbar>
+    </>
+  );
+
+};
 
 export default Header;
