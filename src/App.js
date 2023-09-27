fefbbf3bcd563e545899ece0b93a3003f196bace
wabowen89 <wabowen89@gmail.com>
2023-09-27 15:05:44 -0700

    added react-router-dom. Setup landing page with temp full screen image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from "./logo.svg";
 import { Routes, Route } from "react-router-dom";
+import "./App.css";
 
 // components
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-import "./App.css";
+// pages
+import Landing from "./pages/Landing";
+
+
 
 function App() {
   return (
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,8 +1,27 @@
 import React from "react";
+import TestingImage from "../assets/TestingImage.jpeg";
 
 const Landing = () => {
 
-    
+    const styles = {
+      backgroundImage: `url(${TestingImage})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center center",
+      backgroundRepeat: "no-repeat",
+      width: "100vw", // Set width to viewport width
+      height: "100vh", // Set height to viewport height
+    };
+
+  return (
+
+    <div className="landing-page">
+
+        <div className="full-screen-image"
+        style={styles}></div>
+
+    </div>
+
+  );
 };
 
 export default Landing;
