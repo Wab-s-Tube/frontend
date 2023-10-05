e53194fe74fab1b503149a532101109db9c6a111
wabowen89 <wabowen89@gmail.com>
2023-10-05 15:56:13 -0700

    created basic grid for video display

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,7 +1,9 @@
 import React from "react";
-import TestingImage from "../assets/TestingImage.jpeg";
 import '../styles/Landing.css'
 import { useAppContext } from "../AppContext";
+
+import mockVideo from "../mockData/mockVideoData";
+
 
 const Landing = () => {
 
@@ -9,11 +11,46 @@ const Landing = () => {
 
   const styling = openMenu ? "open" : "closed"
 
+const gridContainer = document.querySelector('.grid-container')
+
+  const test = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    ,
+    "14",
+    ,
+    "15",
+  ];
+
+
+  test.forEach(testing => {
+    const gridItem = document.createElement('div')
+    gridItem.classList.add('video-thumb')
+    gridItem.textContent = testing
+    gridContainer.appendChild(gridItem)
+  })
 
   return (
     <>
       <div className={"landing-" + styling}>
-        <div className="video-thumb"></div>
+        <div className="grid-container">
+          {test.map((itemtest, index) => {
+            <div key={index} classname="video-thumb">
+              {itemtest}
+              </div>
+          })}
+        </div>
       </div>
     </>
   );
